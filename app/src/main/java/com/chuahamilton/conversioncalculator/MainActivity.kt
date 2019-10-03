06ac8506074846228da3fff42e4a40fabc3ed0d2
R.J. Hamilton <robert.hamilton@meijer.com>
2019-10-02 22:52:34 -0400

    Initial commit

---
+++ app/src/main/java/com/chuahamilton/conversioncalculator/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.chuahamilton.conversioncalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gvsu.hamilton.conversioncalculator.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
